9afac77f1ddadec4c4dee267199d8d4e8a6d3a34
Lasat17 <33085898+Lasat17@users.noreply.github.com>
2022-12-29 14:33:57 +0100

    Files added

---
+++ Backend/src/Config/DBSetup.ts
@@ -0,0 +1,3 @@
+export const getDbConnectionString = () => {
+    return "mongodb+srv://lasat17:l130298s@cluster0.z4cbqrv.mongodb.net/?retryWrites=true&w=majority";
+}
---
+++ Frontend/src/common/Constants.ts
@@ -0,0 +1,5 @@
+const constansts = {
+    serverBaseURL: 'https://frontend-ljfkbixlaq-uc.a.run.app'
+}
+
+export default constansts;
